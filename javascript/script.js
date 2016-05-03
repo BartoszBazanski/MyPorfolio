eb62be6516cfcb2141dc29753fd483d91025ecb6
Bartosz Bazanski <bazanski.bartosz@gmail.com>
2016-05-03 13:02:05 +0200

    Add project section

--- javascript/script.js
+++ javascript/script.js
@@ -1,63 +1,13 @@
 $(function(){
-    var top = $("#top");
-    var right = $("#right");
-    var bottom = $("#bottom");
-    var left = $("#left");
-    var menu = $("#round-menu");
-    var menuButtons = $('.menu-button');
-    var description = $('.description');
-    // description.addClass('hidden');
-
-    // menuButtons.hover(function(event){
-    //     $(this).next('.description').addClass('visible').removeClass('hidden')
-    //     },function(){
-    //         $(this).next('.description').addClass('hidden').removeClass('visible');
-    //     });
-
-    top.click(function(){
-        AnimateRotate(menu, 405);
-        AnimateRotate(description, 0);
-        AnimateRotate(description, -45);
+    function slideToSection(section){
+        var id = section.attr('href');
+        $('html, body').animate({
+            scrollTop: $(id).offset().top
+        }, 2000);
+    }
+    var sectionButtons = $('.navigation a');
+    sectionButtons.click(function(e){
+        e.preventDefault();
+        slideToSection($(this));
     })
-    right.click(function(){
-        AnimateRotate(menu, 675);
-        AnimateRotate(description, 0);
-        AnimateRotate(description, 45);
-    })
-    bottom.click(function(){
-        AnimateRotate(menu, 585);
-        AnimateRotate(description, 0);
-        AnimateRotate(description, -225);
-    })
-    left.click(function(){
-        AnimateRotate(menu, 495);
-        AnimateRotate(description, 0);
-        AnimateRotate(description, -135);
-    })
-    //Fading in texts
-    var hello = $('#text-content h2');
-    hello.hide();
-    var greeting = $('#text-content p');
-    greeting.hide();
-    hello.fadeIn(1500);
-    greeting.delay(2000).fadeIn(1500);
-
 });
-function AnimateRotate(obj, angle){
-    var start = getRotationDeg(obj);
-    $({deg: start}).animate({deg: angle}, {
-        duration: 2000,
-        ease: "ease-in",
-        step: function(now){
-            obj.css({
-                transform: 'rotate('+ now +'deg)'
-            });
-        }
-    });
-};
-function getRotationDeg(obj){
-    var matrix = obj.css("transform");
-    var values = matrix.split('(')[1].split(')')[0].split(',');
-    var angle = Math.round(Math.atan2(values[1], values[0]) * (180/Math.PI));
-    return (angle < 0) ? angle + 360 : angle;
-};
